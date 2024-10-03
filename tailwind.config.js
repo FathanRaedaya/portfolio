ec55662f427d4cc96d82d019c2a719230ecb4bca
FathanRaedaya <fy21fnr@leeds.ac.uk>
2024-10-03 20:39:14 +0100

    clean up structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,18 @@
 module.exports = {
-  content: [
-    './*.html',  
-    './*.js',   
-  ],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
       colors: {
         customBackground: '#51434b',
         customBeige: '#f6e0b5',
       },
+    },
+    fontFamily: {
+      'it': ['it', 'sans-serif'],
+      'mont': ['mont', 'sans-serif'],
+      'montM': ['montM', 'sans-serif'],
+      'montL': ['montL', 'sans-serif'],
+      'lmr': ['lmr', 'sans-serif'],
     },
   },
   plugins: [],
